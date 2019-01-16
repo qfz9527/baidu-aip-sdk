866f939c272f8c7066c40779db165e0a081b7b7f
qbhy <96qbhy@gmail.com>
2019-01-16 14:29:07 +0800

    添加laravel支持

---
+++ config/baidu_aip.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: qbhy
+ * Date: 2019/1/16
+ * Time: 下午2:14
+ */
+
+return [
+    'debug'      => env('BAIDU_AIP_DEBUG', false),
+    'app_id'     => env('BAIDU_AIP_APP_ID'),
+    'api_key'    => env('BAIDU_AIP_API_KEY'),
+    'secret_key' => env('BAIDU_AIP_SECRET_KEY'),
+];
---
+++ src/LaravelServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * User: qbhy
+ * Date: 2019/1/16
+ * Time: 下午2:14
+ */
+
+namespace Qbhy\BaiduAIP;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Illuminate\Foundation\Application as LaravelApplication;
+use Laravel\Lumen\Application as LumenApplication;
+
+class LaravelServiceProvider extends BaseServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Setup the config.
+     */
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__ . '/../config/baidu_aip.php');
+        if ($this->app->runningInConsole()) {
+            $this->publishes([$source => base_path('config/baidu_aip.php')], 'baidu_aip');
+        }
+
+        if ($this->app instanceof LumenApplication) {
+            $this->app->configure('baidu_aip');
+        }
+
+        $this->mergeConfigFrom($source, 'baidu_aip');
+    }
+
+    public function register()
+    {
+        $this->app->singleton(BaiduAIP::class, function ($app) {
+            return new BaiduAIP(config('baidu_aip'));
+        });
+
+        $this->app->alias(BaiduAIP::class, 'baidu_aip');
+    }
+}
